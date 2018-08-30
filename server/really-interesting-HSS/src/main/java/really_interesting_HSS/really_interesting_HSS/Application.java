2c37cf45d03f815d6d8f9fb1e109d371ae2cee50
TDM-RE <nye7181@gmail.com>
2018-08-31 01:49:35 +0900

    [UPDATE] 음성 통신 완료

---
+++ server/really-interesting-HSS/src/main/java/really_interesting_HSS/really_interesting_HSS/Application.java
@@ -0,0 +1,10 @@
+package really_interesting_HSS.really_interesting_HSS;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- server/really-interesting-HSS/src/main/java/really_interesting_HSS/really_interesting_HSS/UdpServerHandler.java
+++ server/really-interesting-HSS/src/main/java/really_interesting_HSS/really_interesting_HSS/UdpServerHandler.java
@@ -14,13 +14,10 @@ import io.netty.channel.socket.DatagramPacket;
 
 public class UdpServerHandler extends SimpleChannelInboundHandler<DatagramPacket> {
 
-	//AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100.0F, 16, 2, 4, 44100.0F, false);
-	AudioFormat format = new AudioFormat(16000.0F, 16, 1, true, false);
-	//AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100.0F, 16, 1, 2, 44100.0F, false);
+	AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_UNSIGNED, 16000.0F, 8, 1, 1, 16000.0F, false);
 	DataLine.Info sourceInfo = new DataLine.Info(SourceDataLine.class, format);
 	DataLine.Info targetInfo = new DataLine.Info(TargetDataLine.class, format);
 
-	//byte[] buffer;
 	SourceDataLine source;
 	TargetDataLine target;
 
@@ -37,25 +34,21 @@ public class UdpServerHandler extends SimpleChannelInboundHandler<DatagramPacket
 	@Override
 	public void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg) throws Exception {
 
-		ByteBuf in = Unpooled.buffer();
+		ByteBuf in = Unpooled.buffer(); 
 		ByteBuf out = Unpooled.buffer();
 
 		in = msg.content();
-		//byte[] buffer = new byte[in.readableBytes()];
 		byte[] readBuffer = new byte[in.readableBytes()];
 		byte[] writeBuffer = new byte[in.readableBytes()];
 		System.out.println(in.readableBytes());
 		in.readBytes(readBuffer);
-		//source.write(buffer, 0, buffer.length);
+		
 		source.write(readBuffer, 0, readBuffer.length);
 
-		//target.read(buffer, 0, buffer.length);
 		target.read(writeBuffer, 0, writeBuffer.length);
 		out.writeBytes(writeBuffer, 0, writeBuffer.length);
 
-		//ctx.writeAndFlush(new DatagramPacket(out, msg.sender()));
 		ctx.writeAndFlush(new DatagramPacket(out, msg.sender()));
-		//ctx.writeAndFlush(new DatagramPacket(Unpooled.copiedBuffer(buffer), msg.sender()));
 	}
 
 }
